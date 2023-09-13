978d9e4eb55aab1baff6c1ef8aef24f2929f0aad
suraj Yadav <111379728+suraj826580@users.noreply.github.com>
2023-09-14 01:27:39 +0530

    Form-Validation

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -4,7 +4,8 @@ import { DevTool } from "@hookform/devtools";
 
 export const YoutubeForm = () => {
   const form = useForm();
-  const { register, control, handleSubmit } = form;
+  const { register, control, handleSubmit, formState } = form;
+  const { errors } = formState;
 
   const onSubmit = (data) => {
     console.log(data);
@@ -13,14 +14,20 @@ export const YoutubeForm = () => {
     <div>
       <h1>YouTube Form</h1>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <label htmlFor="username">Username</label>
         <input
           type="text"
           id="username"
           name="username"
-          {...register("username")}
+          {...register("username", {
+            required: {
+              value: true,
+              message: "Please Type your email this is necessary",
+            },
+          })}
         />
+        <p>{errors.username?.message}</p>
 
         <label htmlFor="email">E-mail</label>
         <input type="email" id="email" name="email" {...register("email")} />
@@ -30,8 +37,15 @@ export const YoutubeForm = () => {
           type="text"
           id="channel"
           name="channel"
-          {...register("channel")}
+          {...register("channel", {
+            required: {
+              value: true,
+              message: "Please enter",
+            },
+          })}
         />
+
+        <p>{errors.channel?.message}</p>
 
         <button>Submit</button>
       </form>
